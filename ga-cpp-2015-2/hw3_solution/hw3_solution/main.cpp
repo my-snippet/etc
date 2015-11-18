3b04ea4db4e24b984356ebe3e7585294a73c1b2c
wikibootup <wikibootup@gmail.com>
2015-11-18 19:09:12 +0900

    add xcode version hw3_solution

---
+++ ga-cpp-2015-2/hw3_solution/hw3_solution/main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  hw3_solution
+//
+//  Created by wikibootup on 11/18/15.
+//  Copyright © 2015 wikibootup. All rights reserved.
+//
+
+
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
+
+
+/*
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, World!\n";
+    return 0;
+}
+*/
