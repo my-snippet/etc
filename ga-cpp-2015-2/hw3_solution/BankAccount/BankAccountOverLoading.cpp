586437c63629d532344c89bd8ad412718077ff9b
wikibootup <wikibootup@gmail.com>
2015-11-20 04:19:57 +0900

    bugfix type error about ostream

--- ga-cpp-2015-2/hw3_solution/BankAccount/BankAccountOverLoading.cpp
+++ ga-cpp-2015-2/hw3_solution/BankAccount/BankAccountOverLoading.cpp
@@ -28,14 +28,23 @@ CBankAccount operator+(const CBankAccount& lhs, const CBankAccount& rhs) {
 }
 
 ostream& operator<<(ostream& out, const CBankAccount& obj) {
-    out << "Bank name :" << obj.bankName << "\n";
+    /*	
+     ostream cascading is not allowed to use string type
+     indirect solution about this, copy it to char * variable
+     */
+    char *copyBankName = new char[obj.bankName.length() + 1];
+    strcpy(copyBankName, obj.bankName.c_str());
+    
+    out <<"Bank name :" << copyBankName << "\n";
     out << "Balances :" << obj.balances << "\n";
     out << "InterestRate :" << obj.interestRate << "\n";
-
+    
     int * pasBalances = obj.pastBalances;
-    for(int i = 0 ; i < obj.numOfPastBalances ; i++) {
+    for (int i = 0; i < obj.numOfPastBalances; i++) {
         out << "PastBalanced #" << i << " : " << pasBalances[i] << "\n";
     }
+    
+    delete[] copyBankName;
     
     return out;
 }
