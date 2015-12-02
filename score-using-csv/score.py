7a261b632df370344ee247ff0678d705a008e5f8
wikibootup <wikibootup@gmail.com>
2015-12-02 19:58:05 +0900

    modify score.py : func -> class

--- score-using-csv/score.py
+++ score-using-csv/score.py
@@ -1,24 +1,44 @@
 import csv
 
 
-def get_score_points(path):
-    with open(path) as csvfile:
-        reader = csv.DictReader(csvfile)
-        return next(reader)
-
-
-def get_key(path):
-    with open(path) as csvfile:
-        score_table = {}
-        keyReader = csv.reader(csvfile, delimiter=',', quotechar='|')
-        for col in next(keyReader):
-            score_table.update({col: None})
-        return score_table
-
-
 SCORE_TABLE_PATH='data/sample-score-table.csv'
 SCORE_POINTS_PATH='data/score-points.csv'
-score_table = get_key(SCORE_TABLE_PATH)
-score_points = get_score_points(SCORE_POINTS_PATH)
-print(score_table)
-print(score_points)
+
+
+class ScoreUsingCSV():
+
+    def __init__(self):
+        self.SCORE_TABLE_PATH = SCORE_TABLE_PATH
+        self.SCORE_POINTS_PATH = SCORE_POINTS_PATH
+
+    def get_score_points(self):
+        """
+        It returns dict instance
+        """
+    
+        with open(self.SCORE_POINTS_PATH) as csvfile:
+            reader = csv.DictReader(csvfile)
+            return next(reader)
+    
+    def point_calculator(self, score_line, score_points):
+        score = 9
+
+        for key in score_line.keys():
+            if score_line[key] == '2':
+                score = score - 2
+            elif score_line[key] == '1':
+                score = score - 1
+            elif score_line[key] == 'x':
+                score = score - int(score_points[key])
+
+        if score < 0:
+            score = 0
+
+        return score
+
+    def score(self):
+        with open(self.SCORE_TABLE_PATH, 'r') as csvfile:
+            score_table = csv.DictReader(csvfile)
+            score_points = self.get_score_points()
+            for score_line in score_table:
+                print(self.point_calculator(score_line, score_points))
