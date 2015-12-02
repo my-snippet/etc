137ab873c2bbc3cd59de54a8c2ae069a48961433
wikibootup <wikibootup@gmail.com>
2015-12-02 14:55:03 +0900

    modify send mail : error when empty env

--- python-mailgun/sample_send_mail.py
+++ python-mailgun/sample_send_mail.py
@@ -4,10 +4,10 @@ import os
 
 def send_simple_message():
     return requests.post(
-        os.environ.get("MAILGUN_SENDBOX_DOMAIN"),
+        os.environ["MAILGUN_SENDBOX_DOMAIN"],
         # it's secret api key, never upload, (or conceal this into env)
-        auth=("api", os.environ.get("MAIL_SECRET_KEY")),
-        data={"from": os.environ.get("FROM_EMAIL"),
-              "to": [os.environ.get("FROM_EMAIL")],
+        auth=("api", os.environ["MAIL_SECRET_KEY"]),
+        data={"from": os.environ["FROM_EMAIL"],
+              "to": [os.environ["FROM_EMAIL"]],
               "subject": "Hello",
               "text": "Testing some Mailgun awesomness!"})
