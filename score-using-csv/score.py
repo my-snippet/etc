72d5aad509517b6043ffef275e39754c594bc763
wikibootup <wikibootup@gmail.com>
2015-12-03 00:27:12 +0900

    add method : send mail using result table

--- score-using-csv/score.py
+++ score-using-csv/score.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 import csv
+import os
+
+from sendgrid_email import send_email
 
 
-SCORE_TABLE_PATH='data/sample-score-table.csv'
-SCORE_POINTS_PATH='data/score-points.csv'
+SCORE_TABLE_PATH = 'data/sample-score-table.csv'
+SCORE_POINTS_PATH = 'data/score-points.csv'
+RESULT_TABLE_PATH = 'data/result-table.csv'
 PERFECT_SCORE = 9
 
 
@@ -13,6 +17,7 @@ class ScoreUsingCSV():
         self.SCORE_TABLE_PATH = SCORE_TABLE_PATH
         self.SCORE_POINTS_PATH = SCORE_POINTS_PATH
         self.PERFECT_SCORE = PERFECT_SCORE
+        self.RESULT_TABLE_PATH = RESULT_TABLE_PATH
 
     def get_score_points(self):
         """
@@ -39,30 +44,44 @@ class ScoreUsingCSV():
         return self.PERFECT_SCORE - sum(cut_in_marks.values())
 
     def make_score_comment(self, cut_in_marks):
-        def point_comment(point):
+        def point_comment():
             return str(point) + "점입니다."
 
         def cut_in_marks_comment(comment):
-            comment = comment + "\n\n"
-            comment = comment + "<사유>\n"
+            comment = comment + "<br><br>"
+            comment = comment + "&lt;사유&gt;<br>"
             for key, value in cut_in_marks.items():
                 if value == 'o':
                     pass
-                comment = comment + "'" + str(key) + "'의 사유로" +\
-                    str(value) + "점 감점\n"
+                comment = comment + "'" + str(key) + "'의 사유로 " +\
+                    str(value) + "점 감점<br>"
             return comment
 
         point = self.point_calculator(cut_in_marks)
-        comment = point_comment(point)
+        comment = point_comment()
         if point == self.PERFECT_SCORE:
             return comment
-
         return cut_in_marks_comment(comment)
 
-    def score(self):
+    def write_score_result(self, keys, comment_key="코멘트"):
+        def write_from_score_table():
+            with open(self.RESULT_TABLE_PATH, 'w') as csvfile:
+                writer = csv.DictWriter(csvfile, fieldnames=keys)
+
+                writer.writeheader()
+                for score_line in score_table:
+                    cut_in_marks = self.signsToPoints(score_line, score_points)
+                    score_line[comment_key] = self.make_score_comment(cut_in_marks)
+                    for key in keys:
+                        writer.writerow({key: score_line[key]})       
+
         with open(self.SCORE_TABLE_PATH, 'r') as csvfile:
             score_table = csv.DictReader(csvfile)
             score_points = self.get_score_points()
-            for score_line in score_table:
-                cut_in_marks = self.signsToPoints(score_line, score_points)
-                print(self.make_score_comment(cut_in_marks))
+            write_from_score_table()
+
+    def send_email_from_result(self, comment_key="코멘트"):
+        with open(self.RESULT_TABLE_PATH, 'r') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                send_email(row['코멘트'], [os.environ["DEFAULT_TO"], row['이메일']])
