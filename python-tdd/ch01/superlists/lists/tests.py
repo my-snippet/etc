d6a4d73cb03be229a7f2d576ae69f8a1f72aee39
wikibootup <wikibootup@gmail.com>
2016-04-13 12:08:49 +0900

    update lists test : home html

--- python-tdd/ch01/superlists/lists/tests.py
+++ python-tdd/ch01/superlists/lists/tests.py
@@ -8,3 +8,11 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()  #1
+        response = home_page(request)  #2
+        self.assertTrue(response.content.startswith(b'<html>'))  #3
+        self.assertIn(b'<title>To-Do lists</title>', response.content)  #
+        self.assertTrue(response.content.endswith(b'</html>'))  #
