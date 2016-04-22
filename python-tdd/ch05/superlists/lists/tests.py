af4c166ea379e82e5dedb78513e4eb924ab21126
wikibootup <wikibootup@gmail.com>
2016-04-22 20:03:21 +0900

    init ch5 copied from ch4

---
+++ python-tdd/ch05/superlists/lists/tests.py
@@ -0,0 +1,20 @@
+from django.template.loader import render_to_string
+from django.core.urlresolvers import resolve
+from django.test import TestCase
+from django.http import HttpRequest
+
+from lists.views import home_page
+
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()  #1
+        response = home_page(request)  #2
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
