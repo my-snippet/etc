ef32ebaab7d6ad854da3b9acfdd1b46b0f9f5679
wikibootup <wikibootup@gmail.com>
2016-04-30 22:28:08 +0900

    release skip : fix multivalue error

--- python-tdd/ch05/superlists/lists/tests.py
+++ python-tdd/ch05/superlists/lists/tests.py
@@ -15,7 +15,6 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
-    @unittest.skip("multivalue dict key error : needed to fix")
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()  #1
         response = home_page(request)  #2
