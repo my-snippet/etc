5c15223635366261bf873850420b9d87c0e9b8e7
wikibootup <wikibootup@gmail.com>
2016-05-06 01:16:44 +0900

    --amend

--- python-tdd/ch05/superlists/lists/tests.py
+++ python-tdd/ch05/superlists/lists/tests.py
@@ -89,3 +89,8 @@ class ListViewTest(TestCase):
 
         self.assertContains(response, 'itemey 1') #2
         self.assertContains(response, 'itemey 2') #3
+
+
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
