2fbc5b7f699e1a831e1ce26e672efb4c4d04fdcb
wikibootup <wikibootup@gmail.com>
2016-05-06 17:23:39 +0900

    apply liveserver class, fix table -> tables

--- python-tdd/ch06/superlists/functional_tests/tests.py
+++ python-tdd/ch06/superlists/functional_tests/tests.py
@@ -1,10 +1,9 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
-import time
 
 
-class NewVisitorTest(unittest.TestCase):  #1
+class NewVisitorTest(LiveServerTestCase):  #1
 
     def setUp(self):  #2
         self.browser = webdriver.Firefox()
@@ -19,9 +18,9 @@ class NewVisitorTest(unittest.TestCase):  #1
         self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):  #4
-    # Edith has heard about a cool new online to-do app. She goes
+        # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -62,6 +61,3 @@ class NewVisitorTest(unittest.TestCase):  #1
 
         # She is invited to enter a to-do item straight away
         # [...rest of comments as before]
-
-if __name__ == '__main__':  #7
-    unittest.main(warnings='ignore')  #8
