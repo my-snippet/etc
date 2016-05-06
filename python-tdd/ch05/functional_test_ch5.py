de1d2f5df76f0c4f6da306538419c9420c4e4845
wikibootup <wikibootup@gmail.com>
2016-05-06 16:59:30 +0900

    chap5 seperated chap6

--- python-tdd/ch05/functional_test_ch5.py
+++ python-tdd/ch05/functional_test_ch5.py
@@ -1,9 +1,10 @@
-from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import unittest
+import time
 
 
-class NewVisitorTest(LiveServerTestCase):  #1
+class NewVisitorTest(unittest.TestCase):  #1
 
     def setUp(self):  #2
         self.browser = webdriver.Firefox()
@@ -18,9 +19,9 @@ class NewVisitorTest(LiveServerTestCase):  #1
         self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):  #4
-        # Edith has heard about a cool new online to-do app. She goes
+    # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get(self.live_server_url)
+        self.browser.get('http://localhost:8000')
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -41,8 +42,6 @@ class NewVisitorTest(LiveServerTestCase):  #1
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
-        edith_list_url = self.browser.current_url
-        self.assertRegex(edith_list_url, '/lists/.+') #1
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
@@ -56,31 +55,6 @@ class NewVisitorTest(LiveServerTestCase):  #1
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-        # Francis visits the home page.  There is no sign of Edith's
-        # list
-        self.browser.get(self.live_server_url)
-        page_text = self.browser.find_element_by_tag_name('body').text
-        self.assertNotIn('Buy peacock feathers', page_text)
-        self.assertNotIn('make a fly', page_text)
-
-        # Francis starts a new list by entering a new item. He
-        # is less interesting than Edith...
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Buy milk')
-        inputbox.send_keys(Keys.ENTER)
-
-        # Francis gets his own unique URL
-        francis_list_url = self.browser.current_url
-        self.assertRegex(francis_list_url, '/lists/.+')
-        self.assertNotEqual(francis_list_url, edith_list_url)
-
-        # Again, there is no trace of Edith's list
-        page_text = self.browser.find_element_by_tag_name('body').text
-        self.assertNotIn('Buy peacock feathers', page_text)
-        self.assertIn('Buy milk', page_text)
-
-        # Satisfied, they both go back to sleep
-
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
         # methodical)
@@ -88,3 +62,6 @@ class NewVisitorTest(LiveServerTestCase):  #1
 
         # She is invited to enter a to-do item straight away
         # [...rest of comments as before]
+
+if __name__ == '__main__':  #7
+    unittest.main(warnings='ignore')  #8
