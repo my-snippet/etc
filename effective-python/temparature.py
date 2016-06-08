9446696a49ddc1d3b97f1d10c9899aa68a3abe7f
wikibootup <wikibootup@gmail.com>
2016-06-08 11:43:57 +0900

    property setter ex using temparature

--- effective-python/temparature.py
+++ effective-python/temparature.py
@@ -2,7 +2,17 @@ class Temparature():
 
     def __init__(self, celsius):
         self.celsius = celsius
+        self.farenheit = celsius * (9 / 5) + 32
 
     @property
-    def farenheit(self):
-        return self.celsius * (9 / 5) + 32
+    def temparature(self):
+        return {'celsius': self.celsius, 'farenheit': self.farenheit}
+
+    @temparature.setter
+    def temparature(self, celsius):
+        self.celsius = celsius
+        self.farenheit = celsius * (9 / 5) + 32
+
+    def __repr__(self):
+        self.temparature += 10
+        return ('celsius %d, farenheit %d' % (self.celsius, self.farenheit))
--- effective-python/test_temparature.py
+++ effective-python/test_temparature.py
@@ -8,3 +8,6 @@ class TestTemparature(unittest.TestCase):
     def test_setter_get(self):
         tmpt = Temparature(10)
         self.assertEqual(tmpt.farenheit, 50)
+        tmpt.temparature = 20
+        self.assertEqual(tmpt.celsius, 20)
+        self.assertEqual(tmpt.farenheit, 68)
