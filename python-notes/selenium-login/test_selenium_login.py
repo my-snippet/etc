b6c23f71342c32b4e0e36674f083495fa26dca38
wikibootup <wikibootup@gmail.com>
2016-06-17 21:08:58 +0900

    FT selector login id, password

--- python-notes/selenium-login/test_selenium_login.py
+++ python-notes/selenium-login/test_selenium_login.py
@@ -2,16 +2,34 @@ from selenium_login import login
 
 import unittest
 from os import environ
+from selenium import webdriver
 
 
 class TestSeleniumLogin(unittest.TestCase):
 
     def setUp(self):
-        self.login_url = environ.get('LOGIN_URL')
+        self.url = environ.get('LOGIN_URL')
         self.username = environ.get('ID')
         self.password = environ.get('PASSWORD')
 
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+        self.browser.get(self.url)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_saint_login_id_selectors(self):
+        self.assertTrue(
+            self.browser.find_element_by_id('logonuidfield')
+        )
+
+    def test_saint_login_password_selectors(self):
+        self.assertTrue(
+            self.browser.find_element_by_id('logonpassfield')
+        )
+
     def test_login(self):
         self.assertTrue(
-            login(self.login_url, self.username, self.password)
+            login(self.url, self.username, self.password)
         )
