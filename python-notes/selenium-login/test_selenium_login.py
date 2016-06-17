a811519599e7e0d2d0f0781e670323bb2ae05f8f
wikibootup <wikibootup@gmail.com>
2016-06-17 22:53:30 +0900

    FT saint login in test code

--- python-notes/selenium-login/test_selenium_login.py
+++ python-notes/selenium-login/test_selenium_login.py
@@ -3,6 +3,7 @@ from selenium_login import login
 import unittest
 from os import environ
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 class TestSeleniumLogin(unittest.TestCase):
@@ -29,6 +30,24 @@ class TestSeleniumLogin(unittest.TestCase):
             self.browser.find_element_by_id('logonpassfield')
         )
 
+    @unittest.skip("")
+    def test_saint_login_inputbox_selector(self):
+        self.assertTrue(
+            self.browser.find_element_by_name('login_submit')
+        )
+
+    def test_saint_login(self):
+        id_field = self.browser.find_element_by_id('logonuidfield')
+        id_field.send_keys(self.username)
+        password_field = self.browser.find_element_by_id('logonpassfield')
+        password_field.send_keys(self.password)
+        id_field.send_keys(Keys.ENTER)
+
+        session_text = self.browser.find_element_by_class_name("fontS02")
+        self.assertIn("님 접속을 환영합니다.", session_text.text)
+
+
+    @unittest.skip("")
     def test_login(self):
         self.assertTrue(
             login(self.url, self.username, self.password)
