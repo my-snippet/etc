fc98c2af24f4baa546be85425b95ed5d9d005876
wikibootup <wikibootup@gmail.com>
2016-06-18 23:33:29 +0900

    test suffix finder using set

--- python-notes/umm2/test_suffix_finder.py
+++ python-notes/umm2/test_suffix_finder.py
@@ -8,19 +8,18 @@ import shutil
 class TestSuffixFinder(unittest.TestCase):
 
     def setUp(self):
-        self.path = os.getcwd()
-        self.test_dir = 'test_dir'
-        os.makedirs(self.test_dir)
+        self.dir_path = os.getcwd() + '/' + 'test_dir'
+        os.makedirs(self.dir_path)
 
         self.suffixes = ['.suffix1', '.suffix2']
-        self.suffix1_paths = []
+        self.suffix1_file_paths = []
         for i in range(10):
-            suffix1_path = self.path + '/' + self.test_dir + '/' + \
+            suffix1_path = self.dir_path + '/' + \
                 'file_suffix1_' + str(i) + self.suffixes[0]
-            suffix2_path = self.path + '/' + self.test_dir + '/' + \
+            suffix2_path = self.dir_path + '/' + \
                 'file_suffix2_' + str(i) + self.suffixes[1]
 
-            self.suffix1_paths.append(suffix1_path)
+            self.suffix1_file_paths.append(suffix1_path)
 
             with open(suffix1_path, 'w') as f:
                 pass
@@ -28,8 +27,8 @@ class TestSuffixFinder(unittest.TestCase):
                 pass
 
     def tearDown(self):
-        shutil.rmtree(self.test_dir)
+        shutil.rmtree(self.dir_path)
 
     def test_check_result_and_pre_defined_files_same(self):
-        result = suffix_finder(self.test_dir)
-        assertEqual(set(files), (set(result)))
+        result = suffix_finder(self.dir_path, self.suffixes[0])
+        self.assertEqual(set(self.suffix1_file_paths), (set(result)))
