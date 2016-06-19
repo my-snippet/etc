edc7a45f7e2664ca7e75f84c02c6ec210633c692
wikibootup <wikibootup@gmail.com>
2016-06-19 22:51:03 +0900

    test pingpong return data

--- python-notes/umm2/pingpong/test_pingpong.py
+++ python-notes/umm2/pingpong/test_pingpong.py
@@ -8,7 +8,6 @@ class TestPingpong(unittest.TestCase):
 
     def setUp(self):
         self.pingpong = Pingpong()
-        self.data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 3, 2, 1, 0, 1, 2, 3, 2, 1, 0, -1, 0, 1]
 
     def test_is_change_should_detect_multiples_of_seven(self):
         for i in range(1, 100):
@@ -16,9 +15,9 @@ class TestPingpong(unittest.TestCase):
 
     def test_is_change_should_detect_number_contains_seven(self):
         '''
-        데이터 중에 7이 한개라도 포함되면 그것을 감지할 수 있어야 한다. 예를 
-        들어, 7, 17, ..., 97이나 70, 71, ...79, 또는 700, 701, ...709 등이 
-        해당한다. 
+        데이터 중에 7이 한개라도 포함되면 그것을 감지할 수 있어야 한다. 예를
+        들어, 7, 17, ..., 97이나 70, 71, ...79, 또는 700, 701, ...709 등이
+        해당한다.
         '''
         seven = 7
         seventy = 70
@@ -29,3 +28,9 @@ class TestPingpong(unittest.TestCase):
             self.assertTrue(Pingpong.is_change(seven_hundred+i))
 
         self.assertFalse(Pingpong.is_change(6))
+
+    def test_pingpong_return_data(self):
+        self.assertEqual(Pingpong.pingpong(8), 6)
+        self.assertEqual(Pingpong.pingpong(22), 0)
+        self.assertEqual(Pingpong.pingpong(68), 2)
+        self.assertEqual(Pingpong.pingpong(100), 2)
