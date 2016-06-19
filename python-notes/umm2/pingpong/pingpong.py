c51f8bd5f8ec6fb932d68f878438237d3742b146
wikibootup <wikibootup@gmail.com>
2016-06-19 18:34:07 +0900

    implement is_change detect number contains seven

--- python-notes/umm2/pingpong/pingpong.py
+++ python-notes/umm2/pingpong/pingpong.py
@@ -4,4 +4,6 @@ class Pingpong():
     def is_change(index):
         if (index % 7) == 0:
             return True
+        if str(index).find('7') != -1:
+            return True
         return False
