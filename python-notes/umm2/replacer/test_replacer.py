1d2eceaeeed1e42435455fe6ddb9ab37a18e679f
wikibootup <wikibootup@gmail.com>
2016-06-19 15:23:03 +0900

    comment test replacer scenario

--- python-notes/umm2/replacer/test_replacer.py
+++ python-notes/umm2/replacer/test_replacer.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from replacer import replacer, replacer_f
 
 import unittest
@@ -28,6 +29,16 @@ class TestReplacer(unittest.TestCase):
         os.remove(self.file_name)
 
     def test_replacer_should_affects_only_from_and_to(self):
+        '''
+        텍스트는 10개의 단어로 이루어져 있으며 모두 동일한 단어수를 가집니다.
+        예를 들어, word0, word1, ... word10으로 구성됩니다. 만약 word0를 모두 
+        word1로 바꾼다면 텍스트 내에 word0는 0개이며, word1은 그외 다른 
+        단어 갯수의 2배가 됩니다.
+
+        프로그램은 관여하는 두 단어(바꿀 단어, 바뀔 단어) 외에는 영향을 
+        미쳐서는 안됩니다. 이를 단어 단위로 확인하기로 합니다.
+        '''
+
         from_ = self.words[0]
         to = self.words[1]
         text = replacer(self.text, from_, to)
