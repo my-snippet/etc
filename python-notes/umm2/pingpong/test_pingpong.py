adb93355457d619c869082670e0755a3978a233b
wikibootup <wikibootup@gmail.com>
2016-06-19 18:33:37 +0900

    test is_change detect number contains seven

--- python-notes/umm2/pingpong/test_pingpong.py
+++ python-notes/umm2/pingpong/test_pingpong.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from pingpong import Pingpong
 
 import unittest
@@ -12,3 +13,19 @@ class TestPingpong(unittest.TestCase):
     def test_is_change_should_detect_multiples_of_seven(self):
         for i in range(1, 100):
             self.assertTrue(Pingpong.is_change(i*7))
+
+    def test_is_change_should_detect_number_contains_seven(self):
+        '''
+        데이터 중에 7이 한개라도 포함되면 그것을 감지할 수 있어야 한다. 예를 
+        들어, 7, 17, ..., 97이나 70, 71, ...79, 또는 700, 701, ...709 등이 
+        해당한다. 
+        '''
+        seven = 7
+        seventy = 70
+        seven_hundred = 700
+        for i in range(0, 10):
+            self.assertTrue(Pingpong.is_change(seven+10*i))
+            self.assertTrue(Pingpong.is_change(seventy+i))
+            self.assertTrue(Pingpong.is_change(seven_hundred+i))
+
+        self.assertFalse(Pingpong.is_change(6))
