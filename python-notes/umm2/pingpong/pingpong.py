566f2ae66ce3ada6d14541e525d800d29a51d339
wikibootup <wikibootup@gmail.com>
2016-06-19 22:51:40 +0900

    pingpong functional

--- python-notes/umm2/pingpong/pingpong.py
+++ python-notes/umm2/pingpong/pingpong.py
@@ -1,6 +1,18 @@
 class Pingpong():
 
     @staticmethod
+    def pingpong(index):
+        def iter(count, result, addend):
+            def next_(addend):
+                return result + addend
+            if count < index:
+                if Pingpong.is_change(count):
+                    return iter(count+1, next_(addend * (-1)), addend * (-1))
+                return iter(count+1, next_(addend), addend)
+            return result
+        return iter(1, 1, 1)
+
+    @staticmethod
     def is_change(index):
         def is_multiple_of_seven():
             if (index % 7) == 0:
