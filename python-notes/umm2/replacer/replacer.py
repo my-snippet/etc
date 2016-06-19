4b38dd6c8ed1914f57457e73eff9f075da29c729
wikibootup <wikibootup@gmail.com>
2016-06-19 15:15:50 +0900

    replacer file text in command usage

--- python-notes/umm2/replacer/replacer.py
+++ python-notes/umm2/replacer/replacer.py
@@ -1,7 +1,19 @@
+#!/usr/bin/env python
+import sys
+
+
 def replacer(text, from_, to):
     return text.replace(from_, to)
 
 
-def replacer_f(f, from_, to):
-    with open(f, 'r') as f:
-        return replacer(f.read(), from_, to)
+def replacer_f(file_path, from_, to):
+    result = ''
+    with open(file_path, 'r') as f:
+        result = replacer(f.read(), from_, to)
+
+    print(result)
+    return result
+
+
+if __name__ == "__main__":
+    replacer_f(sys.argv[1], sys.argv[2], sys.argv[3])
